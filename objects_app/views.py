95c558d41364acade8e0b0d790c7b8ab949ad6c6
Andrey Omar Mozo Uscamayta <omar2562@gmail.com>
2022-05-12 11:29:50 -0300

    Adding a custom user

--- objects_app/views.py
+++ objects_app/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse,HttpResponseNotFound
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
+from objects_app.models import *
 import json
+
+
 
 # Create your views here.
 def index(request):
@@ -13,22 +16,22 @@ def index(request):
 		<h2>2022</h2>''')
 
 def users(request):
-	objects = User.objects.all()
+	objects = Person.objects.all()
 	user_list = []
 	for usr in objects:
 		user_list.append(convert_user_object_to_dic(usr))
 	return JsonResponse(user_list, safe=False)
 
 def get_user(request, user_id):
-	user = User.objects.get(pk=user_id)
+	user = Person.objects.get(pk=user_id)
 	return JsonResponse(convert_user_object_to_dic(user), safe=False)
 
 def validate_user(request):
 	print(request.body)
 	body = json.loads(request.body)
-	user = User.objects.get(username=body["username"])
-	if(user.check_password(body["password"])):
-		return JsonResponse(convert_user_object_to_dic(user), safe=False)
+	person = Person.objects.get(user__username=body["username"])
+	if(person.user.check_password(body["password"])):
+		return JsonResponse(convert_user_object_to_dic(person), safe=False)
 	return HttpResponseNotFound('<h1>User not found</h1>')
 
 @csrf_exempt
@@ -43,13 +46,16 @@ def save_user(request):
 		user.first_name = body["first_name"]
 		user.last_name = body["last_name"]
 		user.save()
-		return JsonResponse(convert_user_object_to_dic(user), safe=False)
+		person = Person(phone=body["phone"],user=user)
+		person.save()
+		return JsonResponse(convert_user_object_to_dic(person), safe=False)
 
-def convert_user_object_to_dic(user):
+def convert_user_object_to_dic(person):
 	return {
-			"id":user.id,
-			"username":user.username,
-			"first_name":user.first_name,
-			"last_name":user.last_name,
-			"email":user.email
+			"id":person.id,
+			"username":person.user.username,
+			"first_name":person.user.first_name,
+			"last_name":person.user.last_name,
+			"email":person.user.email,
+			"phone":person.phone
 			}
